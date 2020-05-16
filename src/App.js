247f4cdb0affc593022847761efde4435b4cff48
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2020-05-16 19:18:07 -0300

    pagina do curso javascript

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Navbar from './componentes/navbar/Navbar'
 import { Switch, Route } from 'react-router-dom';
 import Home from './componentes/home/Home';
+import Javascript from './componentes/paginas/JavaScript/Javascriptpagina';
 
 function App() {
   return (
     <>
       <Navbar />
       <Switch>
-        <Route path="/" exact component={Home} />
+        <Route path="/" exact component={Javascript} />
+        <Javascript/>
       </Switch>
       {/* <Footer /> */}
     </>
---
+++ src/componentes/paginas/JavaScript/Javascriptpagina.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+class Javascript extends Component {
+    render() {
+        return (
+            <>
+                <div className="container">
+                <div className="p-5"></div>
+                    <div className="row">
+                        <div className="col-md-9">
+                            <div className="embed-responsive embed-responsive-16by9">
+                                <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/iVmYaIZ5Hmk" allowfullscreen></iframe>
+                            </div>
+                        </div>
+                        <div className="col-md-3">
+                            <div className="card dark">
+                                <div className="card-body">
+                                    <h5 className="text-white">Curso Javascript</h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
+}
+
+export default Javascript
