8c0b928c5564e8da1702c081753816b0458edec2
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2020-05-17 19:04:47 -0300

    Criando navegação do site

--- src/componentes/navbar/Navbar.js
+++ src/componentes/navbar/Navbar.js
@@ -5,24 +5,24 @@ class Navbar extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-lg dark shadow">
-                <a className="navbar-brand" href="#"><h3 className="text-center text-light">DevHipster</h3></a>
+                <Link className="nav-link" to="/"><h3 className="text-center text-light">DevHipster</h3></Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
-                {/* <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
-                            <Link className="nav-link" to="/"><h4 className="text-center text-light">bares</h4> <span className="sr-only">(current)</span></Link>
+                            <Link className="nav-link" to="/"><h4 className="text-center text-light">Cursos</h4> <span className="sr-only">(current)</span></Link>
                         </li>
                          <li className="nav-item">
-                            <Link className="nav-link" to="/restaurantes"><h4 className="text-center text-light">restaurantes</h4></Link>
+                            <Link className="nav-link" to="/exercicios"><h4 className="text-center text-light">Exercícios</h4></Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/baladas"><h4 className="text-center text-light">baladas</h4></Link>
+                            <Link className="nav-link" to="/vagas"><h4 className="text-center text-light">Vagas</h4></Link>
                         </li>
                     </ul>
-                </div> */}
+                </div>
             </nav>
         )
     }
