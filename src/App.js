6ce6d22f3b0cb9155dc99b8d1ddcaffaa12aa83c
Wilgnne Khawan <w.khawanbarbosa@gmail.com>
2020-05-18 19:55:04 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import NavigationBar from './componentes/Navbar'
+import NavigationBar from './componentes/navbar'
 import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Javascript from './pages/JavaScript';
