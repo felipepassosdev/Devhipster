8a01b9222441637fe6ac38a22c0780d0f9ac7a2f
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2020-05-19 20:48:19 -0300

    responsividade

---
+++ web/src/components/CardVagas/index.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+
+import { Container, Card, Row, Col } from 'react-bootstrap';
+import api from '../../services/api'
+
+class CardVagas extends Component {
+    state = {
+        vagas: [],
+    }
+
+    componentDidMount = () => {
+        this.apiVagas()
+    }
+
+    apiVagas = async () => {
+        const response = await api.get(`/repos/frontendbr/vagas/issues`, {
+            params: {
+                state: 'open',
+            },
+        })
+        console.log(response.data)
+
+        this.setState({
+            vagas: response.data
+        })
+    }
+
+
+
+    render() {
+        const { vagas } = this.state;
+        return (
+            <>
+                <Container>
+                    <div className='p-5'></div>
+                    <h3 className="text-white">Vagas Front end</h3>
+                    <Row>
+                        {vagas.map(vaga => (
+                            <Col md={4} key={vaga.id}>
+                                <div className='p-3'></div>
+                                <Card className="dark shadow-lg" style={{ minHeight: '186px', maxHeight: '224px' }}>
+                                    <Card.Text className="text-success">{vaga.state ? 'ABERTA' : 'FECHADA'}</Card.Text>
+                                    <Card.Link href="#">
+                                        <Card.Title className="text-white">{vaga.title}</Card.Title>
+                                    </Card.Link>
+                                    <div className="y">{vaga.labels.map(label => (
+                                        <Card.Title key={label.name} style={{float:'left'}}>
+                                            <div> #{label.name} </div>
+                                        </Card.Title>
+                                    ))}</div>
+                                </Card>
+                            </Col>
+                        ))}
+                    </Row>
+                </Container>
+
+            </>
+        )
+    }
+}
+
+export default CardVagas
--- web/src/pages/Vagas/index.js
+++ web/src/pages/Vagas/index.js
@@ -1,57 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-import { Container, Card, Row, Col } from 'react-bootstrap';
-import api from '../../services/api'
+import CardVagas from '../../components/CardVagas';
 
-class Vagas extends Component {
-    state = {
-        vagas: [],
-    }
+function Vagas() {
 
-    componentDidMount = () => {
-        this.apiVagas()
-    }
-
-    apiVagas = async () => {
-        const response = await api.get(`/repos/frontendbr/vagas/issues`, {
-            params: {
-                state: 'open',
-            },
-        })
-        console.log(response.data)
-
-        this.setState({
-            vagas: response.data
-        })
-    }
-
-
-
-    render() {
-        const { vagas } = this.state;
-        return (
-            <>
-                <Container>
-                    <div className='p-5'></div>
-                    <h3 className="text-white">Vagas Front end</h3>
-                    <Row>
-                        {vagas.map(vaga => (
-                            <Col md={4} key={vaga.id}>
-                                <div className='p-3'></div>
-                                <Card className="dark shadow-lg" style={{ minHeight: '186px' }}>
-                                    <Card.Text className="text-success">{vaga.state ? 'ABERTA' : 'FECHADA'}</Card.Text>
-                                    <Card.Link href="#">
-                                        <Card.Title className="text-white">{vaga.title}</Card.Title>
-                                    </Card.Link>
-                                </Card>
-                            </Col>
-                        ))}
-                    </Row>
-                </Container>
-
-            </>
-        )
-    }
+    return (
+        <>
+            <CardVagas color />
+        </>
+    )
 }
 
 export default Vagas
